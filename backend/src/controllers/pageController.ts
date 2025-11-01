2bbbc56bbd4ad29291cd06ecb88021f5f3c1bcd4
Envigite <benjacontrerasma@yahoo.com>
2025-10-31 22:23:44 -0300

    cambio pageControllers.ts

--- backend/src/controllers/pageController.ts
+++ backend/src/controllers/pageController.ts
@@ -5,26 +5,29 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Ruta absoluta a la carpeta pública del frontend
+const publicPath = path.resolve(__dirname, "../../../frontend/public");
+
 export const renderIndex = (_: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../../frontend/public/index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 };
 
 export const renderCarrito = (_: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../../frontend/public/carrito.html"));
+  res.sendFile(path.join(publicPath, "carrito.html"));
 };
 
 export const renderLogin = (_: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../../frontend/public/login.html"));
+  res.sendFile(path.join(publicPath, "login.html"));
 };
 
 export const renderRegister = (_: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../../frontend/public/register.html"));
+  res.sendFile(path.join(publicPath, "register.html"));
 };
 
 export const renderProduct = (_: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../../frontend/public/product.html"));
+  res.sendFile(path.join(publicPath, "product.html"));
 };
 
 export const renderProfile = (_: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../../frontend/public/profile.html"));
+  res.sendFile(path.join(publicPath, "profile.html"));
 };
